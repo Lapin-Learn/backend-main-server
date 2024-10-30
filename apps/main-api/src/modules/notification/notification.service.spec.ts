69be5309e7c718809535e797f940d17c3ba6385c
NganTrucLe <ngantruc2003@gmail.com>
2024-10-30 18:30:24 +0700

    test(lap-111): mock firebase messaging to pass build

--- apps/main-api/src/modules/notification/notification.service.spec.ts
+++ apps/main-api/src/modules/notification/notification.service.spec.ts
@@ -1,12 +1,14 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { NotificationService } from "./notification.service";
+import { FirebaseMessagingService } from "@app/shared-modules/firebase";
 
+jest.mock("@app/shared-modules/firebase/firebase-messaging.service");
 describe("NotificationService", () => {
   let service: NotificationService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [NotificationService],
+      providers: [NotificationService, FirebaseMessagingService],
     }).compile();
 
     service = module.get<NotificationService>(NotificationService);
