a926a685530ad8120266eaadd18c6749f358b2eb
Bùi Đỗ Duy Quân <21127141@student.hcmus.edu.vn>
2024-12-07 18:54:56 +0700

    feat(lap-495): add pagination interceptor

--- apps/main-api/src/modules/simulated-test/simulated-test.controller.ts
+++ apps/main-api/src/modules/simulated-test/simulated-test.controller.ts
@@ -1,13 +1,27 @@
-import { ClassSerializerInterceptor, Controller, Get, Query, UseInterceptors } from "@nestjs/common";
+import {
+  ClassSerializerInterceptor,
+  Controller,
+  Get,
+  ParseIntPipe,
+  Query,
+  UseGuards,
+  UseInterceptors,
+} from "@nestjs/common";
 import { SimulatedTestService } from "./simulated-test.service";
+import { PaginationInterceptor } from "@app/utils/interceptors";
+import { FirebaseJwtAuthGuard } from "../../guards";
 
+@UseGuards(FirebaseJwtAuthGuard)
 @Controller("simulated-test")
 export class SimulatedTestController {
   constructor(private readonly simulatedTestService: SimulatedTestService) {}
-  @UseInterceptors(ClassSerializerInterceptor)
+
+  @UseInterceptors(ClassSerializerInterceptor, PaginationInterceptor)
   @Get("collections")
-  async getAllSimulatedTest(@Query() params: { offset: number; limit: number }) {
-    const { offset, limit } = params;
+  async getAllSimulatedTest(
+    @Query("offset", ParseIntPipe) offset: number,
+    @Query("limit", ParseIntPipe) limit: number
+  ) {
     return this.simulatedTestService.getAllSimulatedTest(offset, limit);
   }
 }
