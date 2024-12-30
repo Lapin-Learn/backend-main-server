f76c85c936cc7c4bd9083f9f19962a76c9e0a6c6
ducviet5138 <ducviet5138@gmail.com>
2024-12-30 20:45:12 +0700

    chore(lap-623): update migration file, save band score

--- apps/main-api/src/migrations/1735014275871-add_table_speaking_rooms.ts
+++ apps/main-api/src/migrations/1735014275871-add_table_speaking_rooms.ts
@@ -14,17 +14,7 @@ export class AddTableSpeakingRooms1735014275871 implements MigrationInterface {
             default: `uuid_generate_v4()`,
           },
           {
-            name: "profile_id",
-            type: "uuid",
-            isNullable: false,
-          },
-          {
             name: "content",
-            type: "jsonb",
-            isNullable: true,
-          },
-          {
-            name: "evaluation",
             type: "jsonb",
             isNullable: true,
           },
@@ -40,15 +30,6 @@ export class AddTableSpeakingRooms1735014275871 implements MigrationInterface {
             isNullable: false,
             default: "CURRENT_TIMESTAMP",
             onUpdate: "CURRENT_TIMESTAMP",
-          },
-        ],
-        foreignKeys: [
-          {
-            name: "FK_SPEAKING_ROOMS_LEARNER_PROFILES",
-            columnNames: ["profile_id"],
-            referencedColumnNames: ["id"],
-            referencedTableName: "learner_profiles",
-            onDelete: "CASCADE",
           },
         ],
       })
