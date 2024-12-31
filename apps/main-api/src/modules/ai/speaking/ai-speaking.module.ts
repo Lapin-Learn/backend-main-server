bb19cb466252e5fdefaf50e54c6514a8c0589d79
Bùi Đỗ Duy Quân <21127141@student.hcmus.edu.vn>
2025-01-01 00:37:29 +0700

    feat(lap-659): setup bullmq

--- apps/main-api/src/modules/ai/speaking/ai-speaking.module.ts
+++ apps/main-api/src/modules/ai/speaking/ai-speaking.module.ts
@@ -2,10 +2,11 @@ import { Module } from "@nestjs/common";
 import { AISpeakingController } from "./ai-speaking.controller";
 import { AISpeakingService } from "./ai-speaking.service";
 import { GenAIModule } from "@app/shared-modules/genai";
+import { AISpeakingConsumer } from "./ai-speaking.processor";
 
 @Module({
   imports: [GenAIModule],
   controllers: [AISpeakingController],
-  providers: [AISpeakingService],
+  providers: [AISpeakingService, AISpeakingConsumer],
 })
 export class AISpeakingModule {}
--- apps/main-api/src/modules/simulated-test/simulated-test.module.ts
+++ apps/main-api/src/modules/simulated-test/simulated-test.module.ts
@@ -3,9 +3,16 @@ import { SimulatedTestService } from "./simulated-test.service";
 import { SimulatedTestController } from "./simulated-test.controller";
 import { BucketModule } from "../bucket/bucket.module";
 import { GradingModule } from "@app/shared-modules/grading";
+import { BullModule } from "@nestjs/bullmq";
 
 @Module({
-  imports: [BucketModule, GradingModule],
+  imports: [
+    BucketModule,
+    GradingModule,
+    BullModule.registerQueue({
+      name: "evaluate-speaking",
+    }),
+  ],
   providers: [SimulatedTestService],
   controllers: [SimulatedTestController],
 })
