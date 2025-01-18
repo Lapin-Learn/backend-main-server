39dbbb9ac0df1b5d1cb6d0fee091446d75d4411d
Bùi Đỗ Duy Quân <21127141@student.hcmus.edu.vn>
2025-01-18 14:06:02 +0700

    fix: remove log key

--- apps/main-api/src/modules/payment/payment.module.ts
+++ apps/main-api/src/modules/payment/payment.module.ts
@@ -18,8 +18,6 @@ import PayOS from "@payos/node";
         const payOSClientId = configService.get("PAYOS_CLIENT_ID");
         const payOSAPIKey = configService.get("PAYOS_API_KEY");
         const payOSChecksumKey = configService.get("PAYOS_CHECKSUM_KEY");
-
-        console.table([payOSClientId, payOSAPIKey, payOSChecksumKey]);
         const payOS = new PayOS(payOSClientId, payOSAPIKey, payOSChecksumKey);
         return payOS;
       },
