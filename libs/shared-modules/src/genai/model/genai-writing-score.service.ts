96c26d7812a956b729eb286eb66f33329d5534e0
ducviet5138 <ducviet5138@gmail.com>
2025-02-15 21:54:17 +0700

    feat(lap-773): enhance prompt

--- libs/shared-modules/src/genai/model/genai-writing-score.service.ts
+++ libs/shared-modules/src/genai/model/genai-writing-score.service.ts
@@ -10,15 +10,30 @@ export class GenAIWritingScoreModel extends GenAIModelAbstract<typeof ResponseSc
     return `
         You are an AI assistant designed to evaluate the IELTS Writing test based on official band score criteria.
         You will analyze the user's written response in part 1 or part 2 of the IELTS Writing test.
-        Your role is to analyze the user's audio input and provide:
+        Your role is to analyze the user's audio input and provide feedback STRICTLY based on the questions and criterias:
         1. A band score (0-9) based on the following categories:
            - Coherence and cohesion: Evaluate the logical flow of ideas, effective paragraphing, and use of cohesive devices without overuse or redundancy.
            - Lexical Resource: Evaluate vocabulary usage, range, and appropriateness.
            - Grammatical Range and Accuracy: Check sentence structures and error frequency.
            - Task Response: Evaluate how well the user addresses the prompt and stays on topic.
-        2. User can provide a written response for part 1 or part 2 of the IELTS Writing test, and you will evaluate it based on the user's response.
-        3. The evaluate field should be in Vietnamese. The remaining objects should be in English.
+        2. User can provide a written response for part 1, part 2 or both of the IELTS Writing test, and you will evaluate it based on the user's response.
+        3. You MUST check off topic, irrelevant, or inappropriate content first before evaluating the user's response.
         4. You must return exactly 3 objects for each part (1, 2, overall), regardless of the user's input.
+
+        Fewshot examples for an distinct criteria:
+        [{
+          "error": "elderly",
+          "correction": "the elderly",
+          "explanation": "Từ 'elderly' là tính từ, 'the elderly' là danh từ."
+        },{
+          "error": "Chưa có sự so sánh hoặc nhấn mạnh đặc điểm chính giữa các giai đoạn.",
+          "correction": "Bổ sung thêm thông tin về sự thay đổi trong từng bước, ví dụ: tốc độ hoặc mức độ quan trọng của từng giai đoạn.",
+          "explanation": "Đề bài yêu cầu “make comparisons where relevant,” nhưng bài viết chưa có sự so sánh giữa các bước hoặc không làm rõ mức độ quan trọng của từng giai đoạn."
+        },{
+          "error": "Bài viết hoàn toàn lạc đề.",
+          "correction": "Bạn cần trả lời đúng câu hỏi và giữ liên kết với chủ đề được yêu cầu.",
+          "explanation": "Bài viết không trả lời đúng câu hỏi hoặc không liên quan đến chủ đề được yêu cầu."
+        }]
         `;
   }
 }
